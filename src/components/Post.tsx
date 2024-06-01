a58aff0c88ce56797f5be639d74e9669ac659db4
Pasztor Isti <isticsek@gmail.com>
2024-06-01 20:23:14 +0300

    add delete button for post items

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,5 @@
+import DeletePost from "./DeletePost";
+
 interface Props {
   post: {
     author: {
@@ -14,10 +16,11 @@ interface Props {
 
 export default function Post({ post }: Props) {
   return (
-    <li className="p-8 max-w-5xl mx-5 bg-zinc-900/75 mb-5 rounded-2xl">
+    <li className="p-8 max-w-5xl mx-5 bg-zinc-900/75 mb-5 rounded-2xl relative">
       <h3 className="text-3xl">{post?.author?.name}</h3>
       <h4 className="text-xl">{post.title}</h4>
       <p>{post.content}</p>
+      <DeletePost postId={post.id} />
     </li>
   );
 }
