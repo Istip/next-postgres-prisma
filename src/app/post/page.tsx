d6a461cdf2b42b5cb6196a262f4964f2c1fdcfc2
Pasztor Isti <isticsek@gmail.com>
2024-06-01 13:44:14 +0300

    update post creation page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function CreatePost() {
   const emptyState = { title: "", content: "", author: "" };
   const [post, setPost] = useState(emptyState);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
 
   const inputStyles =
     "p-4 border border-zinc-950 bg-zinc-900/75 rounded-xl w-full outline-none focus:ring-2 ring-sky-600";
@@ -22,6 +26,8 @@ export default function CreatePost() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    setLoading(true);
+
     try {
       fetch("/api/post", {
         method: "POST",
@@ -35,11 +41,20 @@ export default function CreatePost() {
       console.error("An error occurred: ", error);
     } finally {
       handleReset();
+      setLoading(false);
+      router.refresh();
     }
   };
 
   return (
-    <div className="grid place-items-center">
+    <div className="grid place-items-center relative">
+      {loading && (
+        <div className="absolute bg-sky-600/25 text-sky-50 w-screen h-screen top-0 left-0 backdrop-blur-sm overflow-hidden">
+          <div className="flex items-center justify-center h-screen animate-pulse">
+            <h3 className="text-3xl font-black">Creating Post</h3>
+          </div>
+        </div>
+      )}
       <div className="container flex items-center justify-between p-5">
         <h3 className="text-3xl">Add Post</h3>
         <Link className="text-sky-600 font-bold" href="/">
@@ -57,6 +72,7 @@ export default function CreatePost() {
           type="text"
           placeholder="Title of the Post"
           onChange={handleChange}
+          disabled={loading}
           required
         />
         <textarea
@@ -66,6 +82,7 @@ export default function CreatePost() {
           rows={5}
           placeholder="Write your content here"
           onChange={handleChange}
+          disabled={loading}
           required
         />
         <input
@@ -75,6 +92,7 @@ export default function CreatePost() {
           type="text"
           placeholder="Enter your name"
           onChange={handleChange}
+          disabled={loading}
           required
         />
         <button
