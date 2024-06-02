0873b6ca4f53815c4452cd42a9b0f7cf4ea1a70f
Pasztor Isti <isticsek@gmail.com>
2024-06-02 10:28:44 +0300

    update delete function

--- src/components/DeletePost.tsx
+++ src/components/DeletePost.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
-export default function DeletePost({ postId }: { postId: string }) {
-  const [loading, setLoading] = useState(false);
+export default function DeletePost({ id }: { id: string }) {
   const router = useRouter();
 
-  const handleRemove = async () => {
-    setLoading(true);
-
+  async function handleRemove() {
     try {
-      await fetch(`/api/post/${postId}`, {
+      await fetch(`/api/removepost/${id}`, {
         method: "DELETE",
       });
 
@@ -19,16 +15,13 @@ export default function DeletePost({ postId }: { postId: string }) {
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error);
-    } finally {
-      setLoading(false);
     }
-  };
+  }
 
   return (
     <button
       onClick={handleRemove}
       className="absolute px-4 py-2 bg-zinc-800 text-zinc-50 right-2 top-2 rounded-lg disabled:cursor-not-allowed disabled:bg-zinc-800/75"
-      disabled={loading}
     >
       x
     </button>
